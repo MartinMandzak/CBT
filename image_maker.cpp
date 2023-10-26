48c38677be55f5a1ffa1d3cebb4615596640beed
MartinMandzak <mandzak.martin3@gmail.com>
2023-10-26 15:01:54 +0200

    There was an attempt :)

---
+++ image_maker.cpp
@@ -0,0 +1,16 @@
+/* IMAGE MAKER
+ *
+ * This program is (hopefully) going to help my dumbass learn how to create a png in c++
+ *
+ */
+
+
+//libraries for png creation
+//figure out how to install opencv lmfao
+//why is this so difficult 
+//kill me
+#include "opencv/cxcore.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+
+
